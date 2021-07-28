4f2ae016dca0e65ff1c04d824978f46376681ff0
chenxiaolani <2524729719@qq.com>
2021-07-28 22:05:53 +0800

    init project

---
+++ src/test/java/com/chenxiaolani/lecmsend/LecmsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxiaolani.lecmsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecmsendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
